6493928ec05d676a1939fa3d8882b2f45832d28c
Md Gulzeesh <gulzeesh2000@gmail.com>
2022-11-09 19:36:45 +0530

    Home top section

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
+const theme = extendTheme({
+  colors: {
+    brand: {
+      100: "#212121",
+    },
+  },
+  button: {
+    _hover: {
+      backgroundColor: "RGBA(0, 0, 0, 0.80)",
+    },
+  },
+});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ChakraProvider theme={theme}>
     <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
