c7e7c94595188ac38817b74eb8d35c6033360308
Ramana Bandapu <bandapu.ramana@gmail.com>
2022-11-10 21:32:18 +0530

    booking

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,13 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./App.css";
 import MainRoutes from "./AllRoutes/MainRoutes";
-
+import Booking from './Pages/Homepage/Booking';
 function App() {
   return (
     <div className="App">
-      <HomeSection/>
-      <MainRoutes />
+      {/* <HomeSection/>
+      <MainRoutes /> */}
+      <Booking/>
     </div>
   );
 }
