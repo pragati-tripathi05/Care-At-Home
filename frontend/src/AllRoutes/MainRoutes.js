dfaf76c8eafe9d0ed49854f6d0237f826a6dc1f6
PRAGATI TRIPATHI <mailpragatihere@gmail.com>
2022-11-11 13:50:54 +0530

    login signup routes

--- frontend/src/AllRoutes/MainRoutes.js
+++ frontend/src/AllRoutes/MainRoutes.js
@@ -7,14 +7,14 @@ import Login from "../Components/Auth/Login";
 
 const MainRoutes = () => {
   return (
-    <div>
+    <>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/booking" element={<Booking />} />
       </Routes>
-    </div>
+    </>
   );
 };
 
