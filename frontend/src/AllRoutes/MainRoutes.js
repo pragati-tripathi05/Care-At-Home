ccee71bafb6a15b673973a1523c917ff32e4ba8e
PRAGATI TRIPATHI <mailpragatihere@gmail.com>
2022-11-11 13:50:25 +0530

    services section

--- frontend/src/AllRoutes/MainRoutes.js
+++ frontend/src/AllRoutes/MainRoutes.js
@@ -2,12 +2,16 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Homepage from "../Pages/Homepage/Homepage";
 import Booking from "../Pages/Booking";
+import Signup from "../Components/Auth/Signup";
+import Login from "../Components/Auth/Login";
 
 const MainRoutes = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/booking" element={<Booking />} />
       </Routes>
     </div>
