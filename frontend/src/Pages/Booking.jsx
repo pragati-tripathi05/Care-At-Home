53477fd03ce1ef272828822779158caff30231ef
Ramana Bandapu <bandapu.ramana@gmail.com>
2022-11-11 15:54:03 +0530

    booking frontend

--- frontend/src/Pages/Booking.jsx
+++ frontend/src/Pages/Booking.jsx
@@ -1,70 +1,87 @@
-import React, { useState } from "react";
-import { Button, Flex, Text } from "@chakra-ui/react";
-import "./Booking.css";
-
+import React from "react";
+import {
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Button,
+  Text,
+} from "@chakra-ui/react";
+import { Link, Navigate } from "react-router-dom";
 const Booking = () => {
-  const [theme, setTheme] = useState();
-
-  const toggleTheme = () => {
-    setTheme((curr) => (theme === curr ? "dark" : "light"));
-  };
-
   return (
-    <div>
-      <Text
-        fontWeight={"bold"}
-        fontFamily={
-          "axiforma-regular,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica,Arial,sans-serif;"
-        }
-        fontSize={"20px"}
-      >
-        My Bookings
-      </Text>
-      <br />
-
-      <Flex
-        border={"1px solid black"}
-        m={"auto"}
-        marginBottom={"150px"}
-        w={"200px"}
-      >
-        <Button
-          className="Ongoing"
-          id="dark"
-          onClick={() => toggleTheme}
-          fontFamily={
-            "axiforma-regular,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica,Arial,sans-serif;"
-          }
-        >
-          ONGOING
-        </Button>
-        <Button
-          className="History"
-          id="light"
-          onClick={() => toggleTheme}
-          fontFamily={
-            "axiforma-regular,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica,Arial,sans-serif;"
-          }
-        >
-          HISTORY
-        </Button>
-      </Flex>
-      <Text>You don't have any projects right now</Text>
-      <br />
-      <Button
-        id="book"
-        fontFamily={
-          "axiforma-regular,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica,Arial,sans-serif;"
-        }
-        pr={"30px"}
-        pl={"30px"}
-        pt={"12px"}
-        pb={"12px"}
-      >
-        BOOK A SERVICE
-      </Button>
-    </div>
+    <>
+      <div className="mybooking_div">
+        <div className="B_tab">
+          <Text
+            fontWeight={"bold"}
+            fontFamily={
+              "axiforma-regular,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica,Arial,sans-serif;"
+            }
+            fontSize={"20px"}
+          >
+            My Bookings
+          </Text>
+          <br />
+          <Tabs variant="unstyled">
+            <TabList
+              className="tablist"
+              m={"auto"}
+              outline={"1px solid black"}
+              w={"223px"}
+              mb={"150px"}
+            >
+              <Tab
+                _selected={{ color: "white", bg: "black", fontWeight: "bold" }}
+                style={{
+                  fontSize: "18px",
+                }}
+                fontFamily={
+                  "axiforma-regular,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica,Arial,sans-serif;"
+                }
+              >
+                ONGOING
+              </Tab>
+              <Tab
+                _selected={{ color: "white", bg: "black", fontWeight: "bold" }}
+                style={{
+                  fontSize: "18px",
+                }}
+                fontFamily={
+                  "axiforma-regular,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica,Arial,sans-serif;"
+                }
+              >
+                HISTORY
+              </Tab>
+            </TabList>
+          </Tabs>
+          <Text
+            fontFamily={
+              "axiforma-regular,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica,Arial,sans-serif;"
+            }
+            fontSize={"18px"}
+          >
+            You don't have any projects right now
+          </Text>
+          <br />
+          <Button
+            id="book"
+            fontFamily={
+              "axiforma-regular,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica,Arial,sans-serif;"
+            }
+            color={"white"}
+            bg={"black"}
+            pr={"30px"}
+            pl={"30px"}
+            pt={"12px"}
+            pb={"12px"}
+          >
+            BOOK A SERVICE
+          </Button>
+        </div>
+      </div>
+    </>
   );
 };
-
 export default Booking;
