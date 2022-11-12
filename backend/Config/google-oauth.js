814f71698f217f71ec44e36cc8388faa49f09e12
PRAGATI TRIPATHI <mailpragatihere@gmail.com>
2022-11-12 16:42:56 +0530

    google oauth

--- backend/Config/google-oauth.js
+++ backend/Config/google-oauth.js
@@ -26,7 +26,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/auth/google/callback",
+      callbackURL: "http://localhost:3000/",
       passReqToCallback: true,
     },
     function (request, accessToken, refreshToken, profile, done) {
