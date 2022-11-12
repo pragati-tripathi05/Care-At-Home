f58d4a6db4b2bba683f942f2f2c9cae7d0f3a498
Ramana Bandapu <bandapu.ramana@gmail.com>
2022-11-12 13:01:15 +0530

    booking data fetch

--- frontend/src/Redux/AppReducer/action.js
+++ frontend/src/Redux/AppReducer/action.js
@@ -2,13 +2,13 @@ import * as types from "./actionTypes";
 import axios from "axios";
 
 const getBookings = (params) => (dispatch) => {
-  // dispatch(getbooksRequest());
+  // dispatch(getbookingsRequest());
   dispatch({ type: types.GET_BOOKING_REQUEST });
 
   axios
     .get("/bookings", params)
     .then((res) => {
-      // console.log("r.data--", r.data);
+    
       return dispatch({ type: types.GET_BOOKING_SUCCESS, payload: res.data });
     })
     .catch((err) => {
